495a436b67f7fb3d5aa397c204cf24652803c1ba
armaango <armaan.goyal@gmail.com>
2017-02-21 16:02:21 -0800

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title:'Harry Potter'},
+        {title:'LOTR'},
+        {title:'FRIENDS'},
+        {title:'JavaScript- the weird parts'}
+    ]
+};
